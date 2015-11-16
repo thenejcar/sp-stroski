6cd485f71fb735d9c17953fae0c75d5f6c63bd3b
Nejc Kisek <nejc.kisek@rettro.si>
2015-11-16 23:41:30 +0100

    se malo napredka

--- js/urediRacun.js
+++ js/urediRacun.js
@@ -1,37 +1,50 @@
+var t = "navaden";
+
 function izbranTip(tip)
 {
     console.log("izbran tip: "+tip);
-    if(tip == navaden)
+    t = tip;
+    if(tip == "navaden")
     {
-        //skrij dodajane uporabnikov
+        //TODO: skrij dodajanje uporabnikov
     }
     else
     {
-
+        $("#container_users").addClass("form-group");
+        $("#container_users").html("<ul role='menu' class='list-group form-group'>" +
+            "<li class='form-control'><input type='checkbox'>Uporabnik1</li>" +
+            "</ul>")
     }
 }
 
 function izpisiOpis(arg)
 {
-    console.log("izpisiOpis "+arg);
     if(arg == "tip")
     {
        $("#opis").html(
             "<h3>Tip: </h3><br/> <ul><li>Navaden Račun: Račun z enim uporabnikom</li>" +
-            "<li>Skupni Prihodki: Deljen račun, kjer vsak član lahko prispeva ali porablja skupni denar.  </li>" +
-            "<li>Skupni Odhodki: Deljen račun, kjer vsi člani prispevajo enak delež denarja, ki se porabi za skupne stroške.</li></ul>");
+            "<li>Skupni Prihodki: Račun z večimi uporabniki, v katerega vsak član lahko prispeva ali jemlje denar.</li>" +
+            "<li>Skupni Odhodki: Račun z večimi uporabniki, ki spremlja porabo denarja za skupne stroške" +
+            " in omogoča enakomerno porazdelitev med vse člane.</li></ul>");
 
-    }
-    else if(arg == "valuta")
-    {
-        $("#opis").html(
-            "<h3>Valuta: </h3><br/> Valuta, ki se bo uporabljala za ta račun."
-        );
     }
     else if(arg == "zacetnostanje")
     {
         $("#opis").html(
             "<h3>Začetno stanje: </h3><br/> Stanje na računu ob odprtju."
+        );
+    }
+    else if(arg == "uporabniki" && tip == "prihodki")
+    {
+        $("#opis").html(
+            "<h3>Uporabniki: </h3><br/> Izbrani uporabniki bodo lahko prispeval denar v račun ali ga iz njega jemali."
+        );
+    }
+    else if(arg == "uporabniki" && tip == "odhodki")
+    {
+        $("#opis").html(
+            "<h3>Uporabniki: </h3><br/> Izberi uporabnike, ki imajo dostop do računa." +
+            " Vsak izbran uporabnik bo prispeval v denar v račun iz katerega se bodo plačevali skupni stroški."
         );
     }
     else
@@ -44,36 +57,43 @@ function izpisiOpis(arg)
 
 $(document).ready(function() {
     //mouseover - opis za vsako polje
-    $("#container_ime").mouseover(function () {
+    $("#container_ime").mouseover(function ()
+    {
         izpisiOpis()
     });
-    $("#container_tip").mouseover(function () {
+    $("#container_tip").mouseover(function ()
+    {
         izpisiOpis("tip")
     });
-    $("#container_valuta").mouseover(function () {
+    $("#container_valuta").mouseover(function ()
+    {
         izpisiOpis("valuta")
     });
-    $("#container_zacetnostanje").mouseover(function () {
+    $("#container_zacetnostanje").mouseover(function ()
+    {
         izpisiOpis("zacetnostanje")
     });
-    //mouseout - pocisti opis
-    $("#container_tip").mouseout(function () {
-        izpisiOpis()
-    });
-    $("#container_valuta").mouseout(function () {
-        izpisiOpis()
-    });
-    $("#container_zacetnostanje").mouseout(function () {
+    $("#container_buttons").mouseover(function ()
+    {
         izpisiOpis()
     });
 
-    $("#racun_tip").focus(function () {
+    //mouseleave - pocisti opis
+    $("#form").mouseleave(function ()
+    {
+        izpisiOpis()
+    });
+
+    $("#racun_tip").focus(function ()
+    {
         izpisiOpis("tip");
     });
-    $("#racun_valuta").focus(function () {
+    $("#racun_valuta").focus(function ()
+    {
         izpisiOpis("valuta");
     });
-    $("#racun_zacetnostanje").focus(function () {
+    $("#racun_zacetnostanje").focus(function ()
+    {
         izpisiOpis("zacetnostanje");
     });
 });
