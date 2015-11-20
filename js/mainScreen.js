6d91dfbf9d183e9a7341182cd016eac6006a97b8
Nejc Kisek <nejc.kisek@rettro.si>
2015-11-20 14:08:28 +0100

    fixed tabs

--- js/mainScreen.js
+++ js/mainScreen.js
@@ -1,5 +1,21 @@
 var chart;
 var canvas;
+var data =
+{
+    labels: ["11.11.2015","12.11.2015","13.11.2015","14.11.2015","15.11.2015","16.11.2015",
+        "17.11.2015","18.11.2015","19.11.2015","20.11.2015","21.11.2015","22.11.2015"],
+    datasets:
+        [{
+            label: "račun 0",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [-15, 10, 5, 25, 15, 25, 45, -5, 5, 25, 15, 30]
+        }]
+};
 
 $(document).ready(function()
 {
@@ -17,34 +33,18 @@ $( window ).resize(function()
 {
     console.log("resize chart")
     canvas = document.getElementById("chart");
-    canvas.width = $("#right_tabs").width();
+    canvas.width = $("#right_tabs").width() - 20;
+    var ctx = $("#chart").get(0).getContext("2d");
+    chart = new Chart(ctx).Line(data, {bezierCurve: false});
 });
 
 function drawChart()
 {
     console.log("drawing chart")
-    var canvas = document.getElementById("chart");
-    canvas.width = $("#right").width();
+    canvas = document.getElementById("chart");
+    canvas.width = $("#right").width() - 20;
 
-
-    var data =
-    {
-        labels: ["11.11.2015","12.11.2015","13.11.2015","14.11.2015","15.11.2015","16.11.2015",
-            "17.11.2015","18.11.2015","19.11.2015","20.11.2015","21.11.2015","22.11.2015"],
-        datasets:
-            [{
-                label: "račun 0",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [-15, 10, 5, 25, 15, 25, 45, -5, 5, 25, 15, 30]
-            }]
-    };
     var ctx = $("#chart").get(0).getContext("2d");
-    if(chart == undefined)
     chart = new Chart(ctx).Line(data, {bezierCurve: false});
 }
 
